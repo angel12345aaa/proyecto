3ec32f3962ad3f339d00c01b1aa647ab5d02f23f
angelgarcia07 <142960087+angelgarcia07@users.noreply.github.com>
2023-10-24 18:35:10 -0600

    inicail

---
+++ SwingAlgoritmosProyecto-main (1)/SwingAlgoritmosProyecto-main/PanelBienvenida/src/txtregis.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+class txtregis {
+
+    static String getPassword() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Object getText() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
